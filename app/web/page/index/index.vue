1b55de8b0a0ce839f47967fdd248edbbb7373338
hezhuopeng <hezhuopeng@hezhuopengdeMacBook-Pro.local>
2018-06-01 11:11:14 +0800

    [optimize] less支持

--- app/web/page/index/index.vue
+++ app/web/page/index/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>123123</div>
+    <div class="title">123123</div>
 </template>
 <script>
 export default {
@@ -11,3 +11,6 @@ export default {
     }
 };
 </script>
+<style scoped lang="less">
+@import './index.less';
+</style>
