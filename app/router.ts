b208d6413e729909c86efd10d282776dcc5a6ae6
hezhuopeng <hezhuopeng@hezhuopengdeMacBook-Pro.local>
2018-06-01 11:33:51 +0800

    [other] 1.node路由更改

--- app/router.ts
+++ app/router.ts
@@ -4,5 +4,5 @@ export default (app: Application) => {
   const { controller, router } = app;
 
   router.get('/', controller.home.index);
-  router.get('/vue', controller.home.tovue);
+  router.get('/app(/.+)?', controller.home.tovue);
 };
